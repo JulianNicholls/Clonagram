37019282da46181eff5830f2390a4d3d0e0dca50
Julian Nicholls <juliannicholls29@gmail.com>
2016-01-13 10:34:28 +0000

    Add following / unfollowing logic.
    user list is now an array of native Swift type Dictionary<String, String>.

--- ParseStarterProject/UserListViewController.swift
+++ ParseStarterProject/UserListViewController.swift
@@ -11,7 +11,7 @@ import Parse
 
 class UserListViewController: UITableViewController {
 
-    var userlist = [NSDictionary]()
+    var userlist = [Dictionary<String, String>]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,20 +27,33 @@ class UserListViewController: UITableViewController {
                 for object in users {
                     if let user = object as? PFUser {
                         if user.objectId != PFUser.currentUser()?.objectId {
-                            self.userlist.append(["name": user.username!, "id": user.objectId!])
+                            let query = PFQuery(className: "follower")
+
+                            query.whereKey("follower", equalTo: (PFUser.currentUser()?.objectId)!)
+                            query.whereKey("following", equalTo: user.objectId!)
+
+                            query.findObjectsInBackgroundWithBlock({
+                                (objects, error) -> Void in
+
+                                var following = "0"
+
+                                if let objects = objects {
+                                    if objects.count > 0 {
+                                        following = "1"
+                                    }
+                                }
+
+                                self.userlist.append(["name": user.username!, "id": user.objectId!, "following": following])
+
+                                self.tableView.reloadData()
+                            })
                         }
                     }
                 }
             }
 
 //            print(self.userlist)
-            self.tableView.reloadData()
         })
-}
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
@@ -56,7 +69,11 @@ class UserListViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
 
-        cell.textLabel!.text = userlist[indexPath.row]["name"] as? String
+        cell.textLabel!.text = userlist[indexPath.row]["name"]
+
+        if userlist[indexPath.row]["following"] == "1" {
+            cell.accessoryType = .Checkmark
+        }
         
         // Configure the cell...
 
@@ -64,61 +81,50 @@ class UserListViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let relation = PFObject(className: "follower")
-
-        relation["following"] = userlist[indexPath.row]["id"] as? String
-        relation["follower"]  = PFUser.currentUser()?.objectId
-
-        relation.saveInBackground()
-
         let cell = self.tableView.cellForRowAtIndexPath(indexPath)
+        let following = userlist[indexPath.row]["following"] == "1"
 
-        cell?.accessoryType = .Checkmark
+        if following {
+            userlist[indexPath.row]["following"] = "0"
+
+            let query = PFQuery(className: "follower")
+
+            query.whereKey("follower", equalTo: (PFUser.currentUser()?.objectId)!)
+            query.whereKey("following", equalTo: userlist[indexPath.row]["id"]!)
+
+            query.findObjectsInBackgroundWithBlock({
+                (objects, error) -> Void in
+
+                if let objects = objects {
+                    for obj in objects {
+                        obj.deleteInBackground()
+                    }
+                }
+            })
+
+            cell?.accessoryType = .None
+        }
+        else {
+            userlist[indexPath.row]["following"] = "1"
+
+            let relation = PFObject(className: "follower")
+
+            relation["following"] = userlist[indexPath.row]["id"]
+            relation["follower"]  = PFUser.currentUser()?.objectId
+
+            relation.saveInBackground()
+
+            cell?.accessoryType = .Checkmark
+        }
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+
+
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
