c5fb415df13391f75215ff3ecbf1437cb09511bc
Julian Nicholls <juliannicholls29@gmail.com>
2016-01-14 09:17:04 +0000

    Add classes for feed view controller and its table cell.

---
+++ ParseStarterProject/TableImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableImageCell.swift
+//  Clonagram
+//
+//  Created by Julian Nicholls on 13/01/2016.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class TableImageCell: UITableViewCell {
+    @IBOutlet weak var postedImage: UIImageView!
+    @IBOutlet weak var caption: UILabel!
+    @IBOutlet weak var username: UILabel!
+}
